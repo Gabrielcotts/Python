497118d75d4bd0bb65b46159bdff3412e3662c12
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-07-09 20:22:05 -0300

    Add files via upload

---
+++ IntroducaoPython/PrimeirosComandos.py
@@ -0,0 +1,38 @@
+#Desafio 1
+nome = input('Qual o seu nome?')
+idade = input('Qual a sua idade?')
+peso = input('Qual o seu peso?')
+
+print('Boas-Vindas ' + nome)
+print('--- Seus dados ---')
+print('Nome: ' + nome )
+print('Idade: ' + idade)
+print('Peso: ' + peso)
+
+#Desafio 2
+print( )
+dia = input('Qual o dia que você nasceu?')
+mes = input('Qual mês você nasceu?')
+ano = input('Qual o ano você nasceu?')
+
+print('--- Dados ---')
+print('Sua data de nascimento ' + dia + '/'+ mes + '/'+ ano)
+
+
+#Desafio 3
+print( )
+num1 = int(input('digite um numero: '))
+num2 = int(input('digite outro numero: '))
+
+print(num1 + num2)
+
+
+
+
+# Aprendendo a diferença entre '+' e ',' em Python
+print( )
+print(7+4)
+print('7'+'4')
+
+
+
