9f3463b7f6dba8f899a43024323ca5222821aa33
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-07-09 20:31:09 -0300

    Refactor input and print statements for clarity
    Updated input prompts and print statements for better readability. Added explanations for string concatenation and comma usage in print.

--- IntroducaoPython/PrimeirosComandos.py
+++ IntroducaoPython/PrimeirosComandos.py
@@ -1,38 +1,41 @@
 #Desafio 1
-nome = input('Qual o seu nome?')
-idade = input('Qual a sua idade?')
-peso = input('Qual o seu peso?')
+nome = input("Qual o seu nome? ")
+idade = input("Qual a sua idade? ")
+peso = input("Qual o seu peso? ")
 
-print('Boas-Vindas ' + nome)
-print('--- Seus dados ---')
-print('Nome: ' + nome )
-print('Idade: ' + idade)
-print('Peso: ' + peso)
+print()
+print("Boas-vindas,", nome)
+print("----- Seus dados -----")
+print("Nome:", nome)
+print("Idade:", idade)
+print("Peso:", peso)
 
 #Desafio 2
 print( )
-dia = input('Qual o dia que você nasceu?')
-mes = input('Qual mês você nasceu?')
-ano = input('Qual o ano você nasceu?')
 
-print('--- Dados ---')
-print('Sua data de nascimento ' + dia + '/'+ mes + '/'+ ano)
+dia = input("Dia de nascimento: ")
+mes = input("Mês: ")
+ano = input("Ano: ")
 
+print("Data de nascimento:", dia + "/" + mes + "/" + ano)
 
 #Desafio 3
 print( )
-num1 = int(input('digite um numero: '))
-num2 = int(input('digite outro numero: '))
 
-print(num1 + num2)
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
 
-
-
+print("Soma:", num1 + num2)
 
 # Aprendendo a diferença entre '+' e ',' em Python
 print( )
+
 print(7+4)
 print('7'+'4')
+
+#Quando usamos '+' precisamos dar um espaço, enquanto a vírgula adiciona automaticamente esse espaço!
+print("A", "B", "C") 
+print("A"+"B"+"C")
 
 
 
